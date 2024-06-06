fa3c73d52287a3308a468b7f0d1b1c47f262c0bb
Denis Karakshev <lty17@mail.ru>
2024-06-06 21:50:58 +0300

    Solved the Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1. 
+// Задайте значения M и N. Напишите 
+// программу, которая выведет все натуральные числа 
+// в промежутке от M до N. Использовать рекурсию, не 
+// использовать циклы.
+// Пример:
+// M = 1; N = 5 -> "1, 2, 3, 4, 5" 
+// M = 4; N = 8 -> "4, 5, 6, 7, 8"
+// M = 8; N = 4 -> "8, 7, 6, 5, 4"
+
+
+
+void PrintNaturalNumbers(int start, int end)
+{
+    if (start < end)
+    {
+        PrintNaturalNumbers2(start, end);
+    }
+    else
+    {
+        PrintNaturalNumbers1(start, end);
+    }
+}
+
+
+void PrintNaturalNumbers1(int start, int end)
+{
+    if (start < end) return;
+    Console.Write(start + " ");
+    PrintNaturalNumbers1(start - 1, end);
+}
+
+
+void PrintNaturalNumbers2(int start, int end)
+{
+    if (start > end) return;
+    Console.Write(start + " ");
+    PrintNaturalNumbers2(start + 1, end);
+}
+
+
+
+Console.WriteLine("Введите число M ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число N ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Натуральные числа от {m} до {n}:");
+PrintNaturalNumbers(m, n);
