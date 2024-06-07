f24e1dbcab93bc5900594034e86822090ecd198c
Denis Karakshev <lty17@mail.ru>
2024-06-07 09:46:06 +0300

    Solved teh Task3

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 3.
+// Задайте произвольный массив. Выведете 
+// его элементы, начиная с конца. Использовать 
+// рекурсию, не использовать циклы.
+// Пример:
+// [1, 2, 5, 0, 10, 34] => 34 10 0 5 2 1
+
+
+
+void Reverse(int[] array, int i)
+{
+    if (i < 0) return;
+    Console.Write(array[i] + " ");
+    Reverse(array, i - 1);
+}
+
+int[] array = { 1, 2, 5, 0, 10, 34 };
+Reverse(array, array.Length - 1);
